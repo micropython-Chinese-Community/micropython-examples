53c45e5a9147c0167ebcda6c629c481ad2f7eac2
shaoziyang <shaoziyang@126.com>
2020-11-22 01:14:12 +0800

    Create BreathLamp2.py

---
+++ boards/bearpi/PWM/BreathLamp2.py
@@ -0,0 +1,18 @@
+'''
+
+    BEARPI Breathing Lamp demo2
+
+'''
+
+from misc import PWM
+from utime import sleep_ms
+
+N = 32
+
+pwm = PWM(PWM.PWM0, 1, N)
+pwm.open()
+
+while 1:
+    for i in range(N*2-1):
+        pwm = PWM(PWM.PWM0, abs(i-N+1)+1, N)
+        sleep_ms(50)
